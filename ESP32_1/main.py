0804b005ce6ab113ed1a480fe93171f0a96cd39a
Anna <annasoestergaard@gmail.com>
2024-06-04 11:18:11 +0200

    updated while loop

--- ESP32_1/main.py
+++ ESP32_1/main.py
@@ -3,11 +3,34 @@ from ili934xnew import ILI9341, color565
 from machine import Pin, SPI, ADC
 import m5stack
 import tt14
+import tt24
 import glcdfont
 import tt14
 import tt32
 import network
 from time import sleep
+from neopixel import NeoPixel
+
+n = 36  
+p = 22  
+np = NeoPixel(Pin(p, Pin.OUT), n)  
+
+def set_color(r, g, b):
+    for i in range(n):
+        np[i] = (r, g, b)
+    np.write()
+
+def clear():
+    set_color(0, 0, 0)
+    
+def red():
+    set_color(255, 0, 0)
+    
+def yellow():
+    set_color(255, 255, 0)
+
+def green():
+    set_color(0, 255, 0)
 
 fonts = [glcdfont,tt14,tt24,tt32]
 
@@ -43,7 +66,7 @@ station.active(True)
 
 esp_now = espnow.ESPNow()
 esp_now.active(True)
-peer = b'\xB0\xA7\x32\xDD\x70\x08'  
+peer = b'\xB0\xA7\x32\xDD\x6F\x30'  
 esp_now.add_peer(peer)
 
 
@@ -53,28 +76,28 @@ adc_pin.atten(ADC.ATTN_11DB)
 pb1 = 0
 pb2 = 0
 pb3 = 0
-pb = Pin(16, Pin.IN)
+pb = Pin(21, Pin.IN)
 
 while True:
+    for i in range(n):
+        if i < 12:
+            np[i] = (0, 255, 0)  # Green
+        elif i > 11 and i < 24:
+            np[i] = (255,0,0)
+        elif i > 23:
+            np[i] = (255, 255, 0)
+    np.write()
     adc_value = adc_pin.read()
     if adc_value > 2600 and adc_value < 2800:
         print("knap 1 trykket")
         pb1 += 1
-        msg = "7c 1"
-        esp_now.send(peer, msg)
+        esp_now.send(peer, "1")
     elif adc_value > 1800 and adc_value < 2000:
         print("knap 2 trykket")
         pb2 += 1
-        msg = "7c 2"
-        esp_now.send(peer, msg)
+        esp_now.send(peer, "2")
     elif pb.value() == 1:
         print("knap 3 trykket")
         pb3 += 1
-        msg = "7c 3"
-        esp_now.send(peer, msg)
+        esp_now.send(peer, "3")
     sleep(3)
-
-
-
-
-
