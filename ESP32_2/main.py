db0e1c5772369c2170ccb70120e94fc667403a7f
Anna <annasoestergaard@gmail.com>
2024-06-18 10:44:37 +0200

    updated esp32 main file

--- ESP32_2/main.py
+++ ESP32_2/main.py
@@ -1,13 +1,22 @@
 from time import sleep
 from machine import Pin, ADC, UART
-
-ldr = ADC(Pin(26))
-ldr.atten(ADC.ATTN_11DB)
+import network
+import espnow
 
 uart = UART(1, baudrate=9600, tx=16, rx=17)
+
 led1 = Pin(14, Pin.OUT)
 
+station = network.WLAN(network.STA_IF)
+station.active(True)
+
+esp_now = espnow.ESPNow()
+esp_now.active(True)
+
 count = 0
+
+ldr = ADC(Pin(35))
+ldr.atten(ADC.ATTN_11DB)  
 
 while True:
     try:
@@ -15,11 +24,14 @@ while True:
         print(ldr_value)
         if ldr_value > 1000:
             count += 1
-            uart.write(str(f"antal kunder {count}".encode()))
             print(count)
+            host, msg = esp_now.recv()
+            if msg is not None and "7c" in msg:
+                print("ESP-NOW Received:", msg)
+                uart.write(bytes(f"{msg} {count}".encode()))
             if count > 10:
                 led1.on()
             sleep(5)
     except Exception as e:
-        print("Error reading LDR:", e)
+        print("Error:", e)
     sleep(1)
